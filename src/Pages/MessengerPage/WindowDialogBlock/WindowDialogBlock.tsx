4ee223cf5d0331950b5f6af46e9e20594e8e495a
Ihar Tsykala <Sudja09@bk.ru>
2020-07-14 18:44:42 +0300

    feat: add header-information in messenger

--- src/Pages/MessengerPage/WindowDialogBlock/WindowDialogBlock.tsx
+++ src/Pages/MessengerPage/WindowDialogBlock/WindowDialogBlock.tsx
@@ -1,25 +1,48 @@
 import React from "react"
 import { connect } from "react-redux"
 import { Typography } from "@material-ui/core"
+import { UserInterface } from "../../../Redux/InterfacesEntity/User.Interface"
+import { MessageInterface } from "../../../Redux/InterfacesEntity/Message.Interface"
 
 type WindowDialogBlockProps = {
   idCurrentUser: number,
+  arrayFriends: UserInterface[],
+  arrayMessages: MessageInterface[][],
   dispatch: any,
 }
 
 const WindowDialogBlock: React.FunctionComponent<WindowDialogBlockProps> = ({
   idCurrentUser,
+  arrayFriends,
+  arrayMessages,
+  dispatch,
 }) => {
   return (
     <div className={"window-dialog-block"}>
       {(idCurrentUser && (
-        <>
-          <div className={"window-dialog-block__header"}></div>
+        <div className={"window-dialog-block__current-dialog-information"}>
+          <div className={"window-dialog-block__header"}>
+            <div className={"window-dialog-block__header-information"}>
+              <Typography
+                variant={"subtitle1"}
+                className={"window-dialog-block__header-information-name"}
+              >
+                {`${arrayFriends[idCurrentUser].name}${arrayFriends[idCurrentUser].id}`}
+              </Typography>
+              <Typography
+                variant={"caption"}
+                className={"window-dialog-block__header-information-members"}
+              >
+                2 members
+              </Typography>
+            </div>
+            <div className={"window-dialog-block__header-menu"}></div>
+          </div>
           <div className={"window-dialog-block__main"}></div>
           <div className={"window-dialog-block__message-field"}></div>
-        </>
+        </div>
       )) || (
-        <div className={"window-dialog-block__empty-window"}>
+        <div className={"window-dialog-block__start-window"}>
           <Typography variant={"subtitle1"} className={"text-select-chat"}>
             Please select chat to start messaging
           </Typography>
@@ -31,6 +54,8 @@ const WindowDialogBlock: React.FunctionComponent<WindowDialogBlockProps> = ({
 
 const mapStateToProps = (state: any) => ({
   idCurrentUser: state.dialog.idCurrentUser,
+  arrayFriends: state.user.arrayFriends,
+  arrayMessages: state.message.arrayMessages,
 })
 
 export default connect(mapStateToProps)(WindowDialogBlock)
