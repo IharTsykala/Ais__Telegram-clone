636d50f9c0a202539d9719a00e30a5398ad918da
Ihar Tsykala <Sudja09@bk.ru>
2020-07-15 05:41:44 +0300

    Updates

---
+++ precache-manifest.5238641a14350ff3e31b171d509aa9d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee4305d424ee1e77008958eabd9d06eb",
+    "url": "/Ais__Telegram-clone/index.html"
+  },
+  {
+    "revision": "9909c22fce2c27f2df5f",
+    "url": "/Ais__Telegram-clone/static/css/main.efc50f3e.chunk.css"
+  },
+  {
+    "revision": "6f314318a2a3dacb1baf",
+    "url": "/Ais__Telegram-clone/static/js/2.ea085158.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Ais__Telegram-clone/static/js/2.ea085158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9909c22fce2c27f2df5f",
+    "url": "/Ais__Telegram-clone/static/js/main.5818a1d1.chunk.js"
+  },
+  {
+    "revision": "4dd08d48d361365f2e8a",
+    "url": "/Ais__Telegram-clone/static/js/runtime-main.ec28c9b3.js"
+  },
+  {
+    "revision": "2fc33436b30b4facb7b0c25bb64f03e6",
+    "url": "/Ais__Telegram-clone/static/media/backgrounForTelegram.2fc33436.jpg"
+  }
+]);
