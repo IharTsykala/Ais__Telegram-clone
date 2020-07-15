cbf7dfffe92f4a6f56fd971a94230fae33bc06ec
Ihar Tsykala <Sudja09@bk.ru>
2020-07-15 05:23:32 +0300

    ref: refactor code App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,13 @@ import "./App.scss"
 import { Provider } from "react-redux"
 import { createStore, applyMiddleware } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension"
-import createSagaMiddleware from "redux-saga"
 import rootReducer from "./Redux/index"
 import MessengerPage from "./Pages/MessengerPage/MessengerPage"
 
-const sagaMiddleware = createSagaMiddleware()
-
 const store = createStore(
   rootReducer,
-  composeWithDevTools({ trace: true, traceLimit: 25 })(
-    applyMiddleware(sagaMiddleware)
-  )
+  composeWithDevTools({ trace: true, traceLimit: 25 })(applyMiddleware())
 )
-//
-// sagaMiddleware.run(cardItemSaga)
-// sagaMiddleware.run(descriptionItemSaga)
 
 const App: React.FC = () => (
   <Provider store={store}>
